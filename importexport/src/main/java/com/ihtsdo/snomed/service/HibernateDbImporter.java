9b65fa07a75fcd2905865f2fe175f4dd0d4245d9
Henrik Pettersen <henrik@sparklingideas.co.uk>
2013-05-02 15:22:19 +0700

    merge hell

--- importexport/src/main/java/com/ihtsdo/snomed/service/HibernateDbImporter.java
+++ importexport/src/main/java/com/ihtsdo/snomed/service/HibernateDbImporter.java
@@ -41,68 +41,66 @@ public class HibernateDbImporter {
     private static final int DEFAULT_CHARACTERISTIC_TYPE = 0;
 
     public Ontology populateDbFromLongForm(String ontologyName, InputStream conceptsStream, 
-            InputStream relationshipsStream, EntityManager em) throws IOException{
-        Stopwatch stopwatch = new Stopwatch().start();
-        LOG.info("Populating database");
+            InputStream relationshipsStream, EntityManager em) throws IOException
+    {
+    	LOG.info("Importing ontology \"" + ontologyName + "\"");
+    	Stopwatch stopwatch = new Stopwatch().start();
+        
         Ontology ontology = createOntology(em, ontologyName);
         populateConcepts(conceptsStream, em, ontology);
         populateLongFormRelationships(relationshipsStream, em, ontology);
-        //em.clear();
         createIsKindOfHierarchy(em, ontology);
+        
         stopwatch.stop();
         LOG.info("Completed import in " + stopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
-        //return ontology;
         return em.find(Ontology.class, ontology.getId());
     }
 
     public Ontology populateDbFromShortForm(String ontologyName, InputStream conceptsStream, 
             InputStream relationshipsStream, EntityManager em) throws IOException
-            {
+    {
+        LOG.info("Importing ontology \"" + ontologyName + "\"");
         Stopwatch stopwatch = new Stopwatch().start();
-        LOG.info("Populating database");        
+        
         Ontology ontology = createOntology(em, ontologyName);
         populateConcepts(conceptsStream, em, ontology);
         populateShortFormRelationships(relationshipsStream, em, ontology);        
-        //em.clear();
         createIsKindOfHierarchy(em, ontology);
+        
         stopwatch.stop();
         LOG.info("Completed import in " + stopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
         return em.find(Ontology.class, ontology.getId());
-            }    
+    }    
 
     protected Ontology createOntology(EntityManager em, final String name) throws IOException {
         final Ontology ontology = new Ontology();
         ontology.setName(name);
         HibernateEntityManager hem = em.unwrap(HibernateEntityManager.class);
         Session session = ((Session) hem.getDelegate()).getSessionFactory().openSession();
-        try {
-            Transaction tx = session.beginTransaction();
-            session.doWork(new Work() {
-                public void execute(Connection connection) throws SQLException {
-                    PreparedStatement statement = connection.prepareStatement(
-                            "INSERT INTO ONTOLOGY (NAME) values (?)", Statement.RETURN_GENERATED_KEYS);
-                    statement.setString(1, name);
-                    int affectedRows = statement.executeUpdate();
-                    if (affectedRows == 0) {
-                        throw new SQLException("Creating ontology failed, no rows affected.");
-                    }
-                    ResultSet generatedKeys = statement.getGeneratedKeys();
-                    if (generatedKeys.next()) {
-                        ontology.setId(generatedKeys.getLong(1));
-                    } else {
-                        throw new SQLException("Creating ontology failed, no generated key obtained.");
-                    }
+        Transaction tx = session.beginTransaction();
+        session.doWork(new Work() {
+            public void execute(Connection connection) throws SQLException {
+                PreparedStatement statement = connection.prepareStatement(
+                        "INSERT INTO ONTOLOGY (NAME) values (?)", Statement.RETURN_GENERATED_KEYS);
+                statement.setString(1, name);
+                int affectedRows = statement.executeUpdate();
+                if (affectedRows == 0) {
+                    throw new SQLException("Creating ontology failed, no rows affected.");
                 }
-            });
-            tx.commit();
-            return ontology;
-        }finally{
-            //session.close();
-        }
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    ontology.setId(generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("Creating ontology failed, no generated key obtained.");
+                }
+            }
+        });
+        tx.commit();
+        return ontology;
     }          
 
     protected void populateConcepts(final InputStream stream, EntityManager em, final Ontology ontology) throws IOException {
-        LOG.info("Populating concepts for ontology [{}({})]", ontology.getName(), ontology.getId());
+        LOG.info("Populating concepts");
         HibernateEntityManager hem = em.unwrap(HibernateEntityManager.class);
         Session session = ((Session) hem.getDelegate()).getSessionFactory().openSession();
         Transaction tx = session.beginTransaction();
@@ -161,9 +159,7 @@ public class HibernateDbImporter {
         setKindOfPredicate(em, ontology);
     }
 
-    protected void setKindOfPredicate(EntityManager em, Ontology o){
-        //        List<Concept> concepts = em.createNamedQuery("SELECT c FROM Concept c", Concept.class).getResultList();
-        //        if (!concepts.isEmpty()) System.out.println(concepts.size());
+    protected void setKindOfPredicate(EntityManager em, Ontology o) throws IllegalStateException{
         try {
             Concept predicate = em.createQuery("SELECT c FROM Concept c WHERE c.serialisedId=" + Concept.IS_KIND_OF_RELATIONSHIP_TYPE_ID + " AND c.ontology.id=" + o.getId(), Concept.class).getSingleResult();
             o.setIsKindOfPredicate(predicate);
@@ -173,7 +169,7 @@ public class HibernateDbImporter {
     }
 
     protected void populateLongFormRelationships(final InputStream stream, final EntityManager em, final Ontology ontology) throws IOException {
-        LOG.info("Populating relationships for ontology [{}({})]", ontology.getName(), ontology.getId());
+        LOG.info("Populating relationships");
         final Map<Long, Long> map = createConceptSerialisedIdMapToDatabaseIdForOntology(ontology, em);
         HibernateEntityManager hem = em.unwrap(HibernateEntityManager.class);
         Session session = ((Session) hem.getDelegate()).getSessionFactory().openSession();
@@ -247,123 +243,107 @@ public class HibernateDbImporter {
             }
         });
         tx.commit();
-
     }
 
     protected void populateShortFormRelationships(final InputStream stream, EntityManager em, final Ontology ontology) throws IOException {
-        LOG.info("Populating relationships for ontology [{}({})]", ontology.getName(), ontology.getId());
+        LOG.info("Populating relationships");
         final Map<Long, Long> map = createConceptSerialisedIdMapToDatabaseIdForOntology(ontology, em);
         HibernateEntityManager hem = em.unwrap(HibernateEntityManager.class);
         Session session = ((Session) hem.getDelegate()).getSessionFactory().openSession();
-        try {
-            Transaction tx = session.beginTransaction();
-            session.doWork(new Work() {
-                public void execute(Connection connection) throws SQLException {
-                    PreparedStatement ps = connection.prepareStatement("INSERT INTO STATEMENT (serialisedid, subject_id, predicate_id, object_id, relationship_group, characteristic_type, refinability, ontology_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-                    try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))){
-                        int currentLine = 1;
-                        String line = null;
-                        try {
-                            line = br.readLine();
-                            //skip the headers
-                            line = br.readLine();
+        Transaction tx = session.beginTransaction();
+        session.doWork(new Work() {
+            public void execute(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement("INSERT INTO STATEMENT (serialisedid, subject_id, predicate_id, object_id, relationship_group, characteristic_type, refinability, ontology_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))){
+                    int currentLine = 1;
+                    String line = null;
+                    try {
+                        line = br.readLine();
+                        //skip the headers
+                        line = br.readLine();
 
-                            while (line != null) {
-                                currentLine++;
-                                if (line.isEmpty()){
-                                    line = br.readLine();
-                                    continue;
-                                }
-                                Iterable<String> split = Splitter.on('\t').split(line);
-                                Iterator<String> splitIt = split.iterator();
-                                try {
-                                    ps.setLong(1, com.ihtsdo.snomed.canonical.model.Statement.SERIALISED_ID_NOT_DEFINED);
-                                    ps.setLong(2, map.get(Long.parseLong(splitIt.next()))); //subject
-                                    ps.setLong(3, map.get(Long.parseLong(splitIt.next())));//predicate
-                                    ps.setLong(4, map.get(Long.parseLong(splitIt.next()))); //object
-                                    ps.setInt(5, Integer.parseInt(splitIt.next()));
-                                    ps.setInt(6, DEFAULT_CHARACTERISTIC_TYPE);
-                                    ps.setInt(7,  DEFAULT_REFINABILITY);
-                                    ps.setLong(8, ontology.getId());
-                                    ps.addBatch();
-                                } catch (NumberFormatException e) {
-                                    LOG.error("Unable to parse line number [" + currentLine + "]. Line was [" + line + "]. Message is [" + e.getMessage() + "]. Skipping entry and continuing", e);
-                                } catch (IllegalArgumentException e){
-                                    LOG.error("Unable to parse line number [" + currentLine + "]. Line was [" + line + "]. Message is [" + e.getMessage() + "]. Skipping entry and continuing", e);
-                                }
+                        while (line != null) {
+                            currentLine++;
+                            if (line.isEmpty()){
                                 line = br.readLine();
+                                continue;
                             }
-                            ps.executeBatch();
-                            LOG.info("Populated [" + (currentLine - 1) + "] relationships");
-                        }finally {
-                            br.close();
+                            Iterable<String> split = Splitter.on('\t').split(line);
+                            Iterator<String> splitIt = split.iterator();
+                            try {
+                                ps.setLong(1, com.ihtsdo.snomed.canonical.model.Statement.SERIALISED_ID_NOT_DEFINED);
+                                ps.setLong(2, map.get(Long.parseLong(splitIt.next()))); //subject
+                                ps.setLong(3, map.get(Long.parseLong(splitIt.next())));//predicate
+                                ps.setLong(4, map.get(Long.parseLong(splitIt.next()))); //object
+                                ps.setInt(5, Integer.parseInt(splitIt.next()));
+                                ps.setInt(6, DEFAULT_CHARACTERISTIC_TYPE);
+                                ps.setInt(7,  DEFAULT_REFINABILITY);
+                                ps.setLong(8, ontology.getId());
+                                ps.addBatch();
+                            } catch (NumberFormatException e) {
+                                LOG.error("Unable to parse line number [" + currentLine + "]. Line was [" + line + "]. Message is [" + e.getMessage() + "]. Skipping entry and continuing", e);
+                            } catch (IllegalArgumentException e){
+                                LOG.error("Unable to parse line number [" + currentLine + "]. Line was [" + line + "]. Message is [" + e.getMessage() + "]. Skipping entry and continuing", e);
+                            }
+                            line = br.readLine();
                         }
-                    } catch (IOException e1) {
-                        LOG.error("Unable to read from the input stream. Bailing out. Message is: " + e1.getMessage(), e1);
-                    } 
-                }
-            });
-            tx.commit();
-        } finally {
-            //session.close();
-        }
+                        ps.executeBatch();
+                        LOG.info("Populated [" + (currentLine - 1) + "] relationships");
+                    }finally {
+                        br.close();
+                    }
+                } catch (IOException e1) {
+                    LOG.error("Unable to read from the input stream. Bailing out. Message is: " + e1.getMessage(), e1);
+                } 
+            }
+        });
+        tx.commit();
     }    
 
     protected Map<Long, Long> createConceptSerialisedIdMapToDatabaseIdForOntology(final Ontology ontology, EntityManager em){
         final HashMap<Long, Long> map = new HashMap<Long, Long>();
         HibernateEntityManager hem = em.unwrap(HibernateEntityManager.class);
         Session session = ((Session) hem.getDelegate()).getSessionFactory().openSession();
-        try {
-            Transaction tx = session.beginTransaction();
-            session.doWork(new Work() {
-                public void execute(Connection connection) throws SQLException {
-                    PreparedStatement ps = connection.prepareStatement("SELECT id, serialisedId FROM concept WHERE ontology_id = ?"); 
-                    ps.setLong(1, ontology.getId());
-                    ResultSet rs = ps.executeQuery();
-                    while (rs.next()){
-                        map.put(rs.getLong(2), rs.getLong(1));
-                    }
+        Transaction tx = session.beginTransaction();
+        session.doWork(new Work() {
+            public void execute(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement("SELECT id, serialisedId FROM concept WHERE ontology_id = ?"); 
+                ps.setLong(1, ontology.getId());
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()){
+                    map.put(rs.getLong(2), rs.getLong(1));
                 }
-            });
-            tx.commit();
-        } finally {
-            //session.close();
-        }
+            }
+        });
+        tx.commit();
         return map;
     }
 
     protected void createIsKindOfHierarchy(EntityManager em, final Ontology o){
         LOG.info("Creating isA hierarchy");
-        //Query query = em.createQuery("SELECT r FROM Statement r WHERE r.ontology.id=" + o.getId());
-        //@SuppressWarnings("unchecked") List<Statement> statements = (List<Statement>) query.getResultList();
-        //final Iterator<Statement> stIt = statements.iterator();
         HibernateEntityManager hem = em.unwrap(HibernateEntityManager.class);
         Session session = ((Session) hem.getDelegate()).getSessionFactory().openSession();
-        try {
-            Transaction tx = session.beginTransaction();
-            session.doWork(new Work() {
-                public void execute(Connection connection) throws SQLException {
-                    PreparedStatement psKindOf = connection.prepareStatement("INSERT INTO KIND_OF (child_id, parent_id) VALUES (?, ?)");
-                    PreparedStatement psStatements = connection.prepareStatement("SELECT subject_id, predicate_id, object_id FROM STATEMENT WHERE ontology_id = ?");
-                    int counter = 1;
-                    psStatements.setLong(1, o.getId());
-                    ResultSet rs = psStatements.executeQuery();
-                    while (rs.next()){
-                        if (rs.getLong(2) == o.getIsKindOfPredicate().getId()){
-                            psKindOf.setLong(1, rs.getLong(1));
-                            psKindOf.setLong(2, rs.getLong(3));
-                            psKindOf.addBatch();
-                            counter++;
-                        }
+        Transaction tx = session.beginTransaction();
+        session.doWork(new Work() {
+            public void execute(Connection connection) throws SQLException {
+                PreparedStatement psKindOf = connection.prepareStatement("INSERT INTO KIND_OF (child_id, parent_id) VALUES (?, ?)");
+                PreparedStatement psStatements = connection.prepareStatement("SELECT subject_id, predicate_id, object_id FROM STATEMENT WHERE ontology_id = ?");
+                int counter = 1;
+                psStatements.setLong(1, o.getId());
+                ResultSet rs = psStatements.executeQuery();
+                while (rs.next()){
+                    if (rs.getLong(2) == o.getIsKindOfPredicate().getId()){
+                        psKindOf.setLong(1, rs.getLong(1));
+                        psKindOf.setLong(2, rs.getLong(3));
+                        psKindOf.addBatch();
+                        counter++;
                     }
-                    psKindOf.executeBatch();
-                    LOG.info("Created [" + counter + "] isA relationships");
                 }
-            });
-            tx.commit();
-        } finally {
-            //session.close();
-        }
+                psKindOf.executeBatch();
+                LOG.info("Created [" + counter + "] isA relationships");
+            }
+        });
+        tx.commit();
     }
 
     protected boolean stringToBoolean(String string){
