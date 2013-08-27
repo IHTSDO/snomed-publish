f56add3a51ff7e57ac9b7ba189001e44eda3612c
Henrik Pettersen <henrik@sparklingideas.co.uk>
2013-08-27 19:11:54 +0100

    Forgot to add new javascript files. See previous commit.

---
+++ web/src/main/webapp/static/js/refsets.js
@@ -0,0 +1,33 @@
+$(document).on("click", ".alert", function(e) {
+    bootbox.alert("Hello world!", function() {
+        console.log("Alert Callback");
+    });
+});
+
+
+function confirmDelete($title, $message, $callback){
+    bootbox.dialog({
+        animate: true,
+        message: $message,
+        height:140,
+        modal: true,
+        resizable: false,
+        title: $title,
+        buttons: {
+          delete_it: {
+            label: "delete",
+            className: "btn-danger",
+            callback: $callback
+          },
+          cancel_it: {
+            label: "cancel",
+            className: "btn-success",
+                callback: function() {}
+          }
+        }
+    });
+}
+
+function test($message){
+
+}
